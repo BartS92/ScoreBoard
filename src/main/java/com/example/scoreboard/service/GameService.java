b6e3a8131094f0e5b8214f8f0d1d81cc3c5cf88c
igor.zavorotnyi <igor.zavorotnyi@gamesys.com>
2024-01-29 14:47:51 +0200

    main
    removed extra, refactoring

--- src/main/java/com/example/scoreboard/service/GameService.java
+++ src/main/java/com/example/scoreboard/service/GameService.java
@@ -2,7 +2,6 @@ package com.example.scoreboard.service;
 
 
 import com.example.scoreboard.domain.Game;
-import com.example.scoreboard.domain.Status;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -12,11 +11,11 @@ public abstract class GameService {
 
     private Game game;
     public void start() {
-        game.setStatus(Status.STARTED);
+
     }
 
     public abstract void update();
     public void finish() {
-        game.setStatus(Status.FINISHED);
+
     }
 }
